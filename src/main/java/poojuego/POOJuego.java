7ff01b05b714ab1b9bb486ac988befa7b2d2c107
elgabo82 <gabrielmorejon@gmail.com>
2023-06-26 13:18:47 -0500

    Creados los paquetes y la estructura base del proyecto.

---
+++ src/main/java/poojuego/POOJuego.java
@@ -0,0 +1,13 @@
+package poojuego;
+
+/**
+ * POO - 2023-S1 26.06.2023 13.16
+ * Ejercicio de Gestión de clases simulando video juegos
+ * @author Gabriel Eduardo Morejón López
+ */
+public class POOJuego {
+
+    public static void main(String[] args) {
+        
+    }
+}
